6e9bb05c7d55bc1140a26e3e7b1acbabdb0366c6
ehgus0117 <fermas0372@naver.com>
2023-03-08 00:00:57 +0900

    0307

--- test.cpp
+++ test.cpp
@@ -1,34 +1,27 @@
 #include <iostream>
-#include <cstring>
+#include <string>
 #include <cstdlib>
 using namespace std;
 
-class number
-{
-private:
-    int num;
-public:
-    number(int n=0): num(n)
-    {
-        cout<<"number(int n=0)"<<endl;
-    }
-    number& operator=(const number& ref)
-    {
-        cout<<"operator=()"<<endl;
-        num=ref.num;
-        return *this;
-    }
-    operator int ()
-    {
-        return num;
-    }
-    void shownuber() { cout<<num<<endl;}
-};
 int main(void)
 {
-    number num1;
-    num1= 30;
-    number num2=num1+20;
-    num2.shownuber();
+    string str1="I like ";
+    string str2="String class";
+    string str3 = str1+str2;
+    
+    cout<<str1<<endl;
+    cout<<str2<<endl;
+    cout<<str3<<endl;
+    
+    str1+=str2;
+    if(str1==str3)
+        cout<<"동일 문자영!"<<endl;
+    else
+        cout<<"동일하지 않은 문자열!"<<endl;
+    
+    string str4;
+    cout<<"문자열 입력: ";
+    cin>>str4;
+    cout<<"입력한 문자열: "<<str4<<endl;
     return 0;
 }
